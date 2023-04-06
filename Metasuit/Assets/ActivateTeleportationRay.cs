2d305029c431eba34602dbac15da472befb25021
metasuit <116499608+metasuit@users.noreply.github.com>
2023-04-06 18:33:32 +0200

    added menu
    added menu tracking with headset

--- Metasuit/Assets/ActivateTeleportationRay.cs
+++ Metasuit/Assets/ActivateTeleportationRay.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.InputSystem;
+using System.Collections.Specialized;
 
 public class ActivateTeleportationRay : MonoBehaviour
 {
@@ -15,10 +16,16 @@ public class ActivateTeleportationRay : MonoBehaviour
     public InputActionProperty leftCancel;
     public InputActionProperty rightCancel;
 
+    public XRRayInteractor leftRay;
+    public XRRayInteractor rightRay;
+
     // Update is called once per frame
     void Update()
     {
-        leftTeleportation.SetActive(leftCancel.action.ReadValue<float>() == 0 && leftActivate.action.ReadValue<float>() > 0.1f);
-        rightTeleportation.SetActive(rightCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1f);
+        bool isLeftRayHovering = leftRay.TryGetHitInfo(out Vector3 leftPos, out Vector3 leftNormal, out int leftNumber, out bool leftValid);
+        bool isRightRayHovering = rightRay.TryGetHitInfo(out Vector3 rightPos, out Vector3 rightNormal, out int rightNumber, out bool rightValid);
+
+        leftTeleportation.SetActive(!isLeftRayHovering && leftCancel.action.ReadValue<float>() == 0 && leftActivate.action.ReadValue<float>() > 0.1f);
+        rightTeleportation.SetActive(!isRightRayHovering && rightCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1f);
     }
 }
